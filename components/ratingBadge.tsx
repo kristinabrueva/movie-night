164e754293b3bcebf9152f19f565a4f4eb67d32f
Kristina Brueva <kristinabruuu@gmail.com>
2023-02-16 11:57:01 +1100

    Create rating badge component

---
+++ components/ratingBadge.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const RatingBadge: React.FunctionComponent<{
+  rating: number;
+}> = ({ rating }) => {
+  const ratingToPerc = rating ? (rating * 10).toString() + "%" : "NR";
+  return (
+    <div className="text-xs w-10 h-10 rounded-full leading-10 bg-black text-white absolute left-2 -bottom-5 border-yellow-400 border text-center">
+      {ratingToPerc}
+    </div>
+  );
+};
+
+export default RatingBadge;
