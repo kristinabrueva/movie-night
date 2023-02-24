6fd15f6d55d2f5e47131ba8ef8a845738dfb22f6
Kristina Brueva <kristinabruuu@gmail.com>
2023-02-24 16:36:29 +1100

    Create getMovieGenres by ids

--- helpers/helpers.ts
+++ helpers/helpers.ts
@@ -1,21 +1,43 @@
 import { MovieType } from "../types";
 
-export const formatDate = (originalDate: string): string => {
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
+const genres = [
+  { id: 28, name: "Action" },
+  { id: 12, name: "Adventure" },
+  { id: 16, name: "Animation" },
+  { id: 35, name: "Comedy" },
+  { id: 80, name: "Crime" },
+  { id: 99, name: "Documentary" },
+  { id: 18, name: "Drama" },
+  { id: 10751, name: "Family" },
+  { id: 14, name: "Fantasy" },
+  { id: 36, name: "History" },
+  { id: 27, name: "Horror" },
+  { id: 10402, name: "Music" },
+  { id: 9648, name: "Mystery" },
+  { id: 10749, name: "Romance" },
+  { id: 878, name: "Science Fiction" },
+  { id: 10770, name: "TV Movie" },
+  { id: 53, name: "Thriller" },
+  { id: 10752, name: "War" },
+  { id: 37, name: "Western" },
+];
 
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+export const formatDate = (originalDate: string): string => {
   const date = originalDate.split("-").reverse();
   return date[0] + " " + months[parseInt(date[1]) - 1] + ", " + date[2];
 };
@@ -63,3 +85,6 @@ export const sortTitleDesc = (movies: MovieType[]) => {
     ),
   };
 };
+
+export const getMovieGenres = (genre_ids: number[]) =>
+  genre_ids.map((id) => genres.find((g) => g.id === id)?.name);
