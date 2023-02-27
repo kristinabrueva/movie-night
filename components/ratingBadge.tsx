01bf5abdbb195ce9883b31b05f5e60c5885b42ed
Kristina Brueva <kristinabruuu@gmail.com>
2023-02-28 10:16:19 +1100

    Dinamic border color on RatingBadge

--- components/ratingBadge.tsx
+++ components/ratingBadge.tsx
@@ -1,11 +1,24 @@
+import clsx from "clsx";
 import React from "react";
 
 export const RatingBadge: React.FunctionComponent<{
   rating: number;
 }> = ({ rating }) => {
   const ratingToPerc = rating ? Math.round(rating * 10).toString() + "%" : "NR";
+
+  const borderColor = () => {
+    if (rating >= 8) return "border-green-400";
+    else if (rating >= 6) return "border-yellow-400";
+    else return "border-red-400";
+  };
+
   return (
-    <div className="text-xs w-10 h-10 rounded-full leading-10 bg-black text-white absolute left-2 -bottom-5 border-yellow-400 border text-center">
+    <div
+      className={clsx(
+        "text-xs w-10 h-10 rounded-full leading-10 bg-black text-white border-re absolute left-2 -bottom-5 border text-center",
+        `${borderColor()} `
+      )}
+    >
       {ratingToPerc}
     </div>
   );
